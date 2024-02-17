92db5d194fd830ad80cd4d5ae6b70d532825feea
Md Fahimuddin Ansari <striverfahim@gmail.com>
2024-02-17 14:53:59 +0530

    4th change carousel

--- script.js
+++ script.js
@@ -78,10 +78,11 @@ const interval = setInterval(autoChangeImage, 2500);
 
   // Slide images according to the slide button clicks
   slideButtons.forEach(button => {
+      
       button.addEventListener("click", () => {
           clearInterval(interval);
           const direction = button.id === "prev-slide" ? -1 : 1;
-          const scrollAmount = imageList.clientWidth * direction/1.5;
+          const scrollAmount = (imageList.clientWidth * direction);
           imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
       });
   });
